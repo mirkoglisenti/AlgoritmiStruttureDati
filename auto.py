1e4a8de9394c3688a3e615b1b6c0ab9d9ff431fa
Mirko Glisenti <mirko.glisenti1999@gmail.com>
2022-12-05 16:46:08 +0100

    requirements.txt

--- auto.py
+++ auto.py
@@ -7,9 +7,6 @@ import gc
 #print(sys.getrecursionlimit())
 sys.setrecursionlimit(1000000000)
 
-
-plt.figure(figsize = (12, 8), dpi = 300)
-
 for m in range(3, 10):
     N_arr = []
     exec_arr = []
@@ -25,13 +22,25 @@ for m in range(3, 10):
         N_arr.append(n)
         visited_arr.append(max_visited)
         exec_arr.append(max_exec)
-    plt.subplot(5, 2, m%10+1)
+    # plt.subplot(5, 2, m%10+1)
+    plt.figure(figsize=(12, 8), dpi=300)
     plt.semilogy(N_arr, exec_arr, "-g")
     plt.semilogy(N_arr, N_arr, '-r')
     plt.semilogy(N_arr, list(map(lambda x:pow(x,2),N_arr)), '-b')
-    plt.title('M Card = ' + str(m))
+    plt.title('Execution time - M Card = ' + str(m))
     plt.xlabel('N cardinality')
-    plt.ylabel('ms')
+    plt.ylabel('Execution time [ms]')
+    plt.tight_layout()
+    plt.savefig('img/exec_M_' + str(m) + '.png')
+    plt.figure(figsize=(12, 8), dpi=300)
+    plt.semilogy(N_arr, visited_arr, "-g")
+    plt.semilogy(N_arr, N_arr, '-r')
+    plt.semilogy(N_arr, list(map(lambda x: pow(x, 2), N_arr)), '-b')
+    plt.title('Visited nodes - M Card = ' + str(m))
+    plt.xlabel('N cardinality')
+    plt.ylabel('Visited nodes')
+    plt.tight_layout()
+    plt.savefig('img/visited_M_' + str(m) + '.png')
     del N_arr
     del exec_arr
     gc.collect()
@@ -41,5 +50,3 @@ for m in range(3, 10):
 #plt.plot(N_arr, exec_arr, "-g")
 #plt.subplot(1, 2, 2)
 #plt.plot(N_arr, visited_arr, "-r")
-plt.tight_layout()
-plt.savefig('test.png')
